d161c08c0e03e0ed5761bfda4230633c7fbb615f
Ebong Michael <ebongmichael@gmail.com>
2022-09-14 09:01:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+nclude "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet 
+ *
+ *  */
+
+
+
+void print_alphabet(void)
+
+{
+
+		int ch;
+
+
+
+			for (ch = 'a'; ch <= 'z'; ++ch)
+
+						_putchar(ch);
+
+				_putchar('\n');
+
+}
