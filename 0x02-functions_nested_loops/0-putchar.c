08885d0b8997c616e2163983fef61d9b916e60ed
Ebong Michael <ebongmichael@gmail.com>
2022-09-14 14:03:29 -0700

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,43 @@
-#include "holberton.h"
+#include "main.h"
+
+
 
 /**
- *
- *  * main - prints the string "Holberton" from a character array.
- *
- *   * Return: 0 on success.
- *
- *    */
+
+ * main - print putchar
+
+ * Return: always 0
+
+ */
+
+
 
 int main(void)
 
 {
 
-		char c[] = "Holberton";
+	_putchar('_');
 
-			int i;
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
 
 
 
-				for (i = 0; i < 9; i++)
+	return (0);
 
-						{
 
-									_putchar(c[i]);
-
-										}
-
-					_putchar('\n');
-
-						return (0);
 
 }
