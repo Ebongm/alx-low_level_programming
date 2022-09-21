5e525064923410f2077ad3ef612f745f93a37c07
Ebong Michael <ebongmichael@gmail.com>
2022-09-21 11:46:05 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,13 @@
 #include "main.h"
 
 /**
-
  * _strncpy - copies a string
-
  * @dest: destination.
-
  * @src: source.
-
  * @n: amount of bytes from src.
-
  * Return: the pointer to dest.
 
  */
-
 
 
 char *_strncpy(char *dest, char *src, int n)
@@ -25,16 +19,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-
 		dest[i] = src[i];
-
 	for ( ; i < n; i++)
 
 		dest[i] = '\0';
 
-
-
 	return (dest);
-
 }
 
