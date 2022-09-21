50bd79f21323fcfd8c6972af61fb219d7ffe4b1b
Ebong Michael <ebongmichael@gmail.com>
2022-09-21 11:33:34 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,54 +1,54 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *
- *  * _strncat - concatenates two strings,
- *
- *   * @dest: destination.
- *
- *    * @src: source.
- *
- *     * @n: amount of bytes used from src.
- *
- *      * Return: the pointer to dest.
- *
- *       */
+
+ * _strncat - concatenates two strings,
+
+ * @dest: destination.
+
+ * @src: source.
+
+ * @n: amount of bytes used from src.
+
+ * Return: the pointer to dest.
+
+ */
 
 char *_strncat(char *dest, char *src, int n)
 
 {
 
-		int count = 0, count2 = 0;
+	int count = 0, count2 = 0;
 
 
 
-			while (*(dest + count) != '\0')
+	while (*(dest + count) != '\0')
 
-					{
+	{
 
-								count++;
+		count++;
 
-									}
+	}
 
 
 
-				while (count2 < n)
+	while (count2 < n)
 
-						{
+	{
 
-									*(dest + count) = *(src + count2);
+		*(dest + count) = *(src + count2);
 
-											if (*(src + count2) == '\0')
+		if (*(src + count2) == '\0')
 
-															break;
+			break;
 
-													count++;
+		count++;
 
-															count2++;
+		count2++;
 
-																}
+	}
 
-					return (dest);
+	return (dest);
 
 }
 
