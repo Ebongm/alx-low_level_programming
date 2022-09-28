d82010237cd567eb76204a09d7e3489d588e87aa
Ebong Michael <ebongmichael@gmail.com>
2022-09-28 12:37:04 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,33 +1,18 @@
-/**                                                             
- *
- *  * _strlen - returns the length of a string                     
- *
- *   *                                                              
- *
- *    * @s: string parameter input                                   
- *
- *     *                                                              
- *
- *      * Return: length of string                                     
- *
- *      */                                                              
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
 
-                                                                
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
 
-int _strlen(char *s)                                            
-
-{                                                               
-
-	        int counter;                                            
-
-		                                                                
-
-		        for (counter = 0; *s != '\0'; ++s)                      
-
-				                ++counter;                                      
-
-			                                                                
-
-			        return (counter);                                       
-
-} 
+	return (longi);
+}
